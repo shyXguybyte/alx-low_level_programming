23203834da0455284c77ac0284966dc67d16a431
shyXguybyte <shehab2003magdy@gmail.com>
2023-08-11 12:27:45 -0700

    g

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+/**
+ * main - Entry point
+ *
+ * description: a c program
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}}
