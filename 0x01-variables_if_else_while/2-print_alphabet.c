a39b341b350382fe14c2837e73a174448187897e
shyXguybyte <shehab2003magdy@gmail.com>
2023-08-11 11:41:35 -0700

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: a c program
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	for (int ch = 'a'; ch < 'z' + 1 ; ch++)
+	{
+		putchar (ch);
+		return (0);
+	}
+}
