94183fa7a86b4835313648f0358e3f36838bb0a4
shyXguybyte <shehab2003magdy@gmail.com>
2023-08-11 12:29:28 -0700

    g

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * description: a c program
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
