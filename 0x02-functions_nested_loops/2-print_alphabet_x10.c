eb8815d153720a0a9257825ab8dece11685aaac6
shyXguybyte <shehab2003magdy@gmail.com>
2023-08-15 05:34:30 -0700

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main."
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int counter = 10;
+
+	while (counter != 0)
+	{
+		print_alphabet();
+		_putchar('\n');
+		counter--;
+	}
+}
+
