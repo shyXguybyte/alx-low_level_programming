4fe15a627e5e815381e2a993c9f9c48d5a0f22a1
shyXguybyte <shehab2003magdy@gmail.com>
2023-08-25 11:20:02 -0700

    l

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcat - concatinate 2 strings
+ * @dest: first string
+ * @src: second string
+ * Return: the result of 2 added strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	_putchar(*dest);
+	_putchar(*src);
+	_putchar('\n');
+}
