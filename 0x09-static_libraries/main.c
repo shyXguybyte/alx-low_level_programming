94c912ddb5241d30b5ba1e5e4d46840661945b05
shyXguybyte <shehab2003magdy@gmail.com>
2023-09-03 07:18:05 -0700

    l

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * main - entry point
+ *
+ * Return: a quote
+ */
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
