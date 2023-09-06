f2795f14e1918898132a737b253eca155e3f9598
shyXguybyte <shehab2003magdy@gmail.com>
2023-09-06 04:43:59 -0700

    l

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * main - entry point
  * @argc: length of array
  * @argv: array of Strings
- * 
+ *
  * Return: multiply of x and y
  */
 
 int main(int argc, char *argv[])
 {
-    int x, y;
-     
-        if (argc < 3)
-        {   
-            printf("Error\n");
-            return (1);
-        }
-        else
-        {   
-            x = atoi(argv[1]);
-            y = atoi(argv[2]);
-            printf("%d\n", x * y);
+	int x, y;
 
-            return (0);
-        }
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d\n", x * y);
+		return (0);
+	}
 }
