12c716aab7077fa4f481f3d3a0de528ef0c29390
shyXguybyte <shehab2003magdy@gmail.com>
2023-09-06 02:32:33 -0700

    l

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: length of array argv
+ * @argv: array of strings
+ *
+ * Return: name of the prog
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+}
