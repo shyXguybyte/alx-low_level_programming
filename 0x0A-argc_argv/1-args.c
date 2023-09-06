d511426b1f45240f538d7530abe56460a70dd06b
shyXguybyte <shehab2003magdy@gmail.com>
2023-09-06 02:41:22 -0700

    l

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: length of array
+ * @argv: the array
+ *
+ * Return: length of array
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%i\n", argc);
+	return (0);
+}
