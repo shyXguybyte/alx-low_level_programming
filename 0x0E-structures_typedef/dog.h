2031a3556dc17eef5be7aafe7e57085e12f32d9a
shyXguybyte <shehab2003magdy@gmail.com>
2023-09-14 02:40:24 -0700

    l

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+ * struct dog - my struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: bla bla
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
